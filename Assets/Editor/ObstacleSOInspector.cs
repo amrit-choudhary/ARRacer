108212630fdd9a7082203b5f35de2bfdd9aa2bd1
amrit-choudhary <albela.amrit@gmail.com>
2018-03-23 05:43:46 +0530

    obstacles created

---
+++ Assets/Editor/ObstacleSOInspector.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ObstacleSO))]
+public class ObstacleSOInspector : Editor {
+
+    ObstacleSO obstacleSO;
+
+    public void OnEnable() {
+        obstacleSO = (ObstacleSO)target;
+    }
+
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+        EditorGUILayout.Space();
+        
+        EditorGUILayout.LabelField(Convert(0) + Convert(1) + Convert(2));
+        EditorGUILayout.LabelField(Convert(3) + Convert(4) + Convert(5));
+        EditorGUILayout.LabelField(Convert(6) + Convert(7) + Convert(8));
+    }
+
+    private string Convert(int index) {
+        if (obstacleSO.obstacle.list[index])
+            return "\u2588" + "\u0020";
+        else
+            return "\u2000" + "\u2000";
+    }
+
+}
---
+++ Assets/Scripts/Helpers.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helpers {
+
+	public static Obstacle Rotate90(Obstacle input) {
+        Obstacle output = new Obstacle(input);
+
+        output.list[0] = input.list[6];
+        output.list[1] = input.list[3];
+        output.list[2] = input.list[0];
+
+        output.list[3] = input.list[7];
+        //output.list[4] = input.list[4];
+        output.list[5] = input.list[1];
+
+        output.list[6] = input.list[8];
+        output.list[7] = input.list[5];
+        output.list[8] = input.list[2];
+
+        return output;
+    }
+
+    public static Obstacle MirrorHorizontal(Obstacle input) {
+        Obstacle output = new Obstacle(input);
+
+        output.list[0] = input.list[6];
+        output.list[1] = input.list[7];
+        output.list[2] = input.list[8];
+
+        output.list[3] = input.list[3];
+        //output.list[4] = input.list[4];
+        output.list[5] = input.list[5];
+
+        output.list[6] = input.list[0];
+        output.list[7] = input.list[1];
+        output.list[8] = input.list[2];
+
+        return output;
+    }
+
+    public static Obstacle MirrorVertical(Obstacle input) {
+        Obstacle output = new Obstacle(input);
+
+        output.list[0] = input.list[2];
+        output.list[1] = input.list[1];
+        output.list[2] = input.list[0];
+
+        output.list[3] = input.list[5];
+        //output.list[4] = input.list[4];
+        output.list[5] = input.list[3];
+
+        output.list[6] = input.list[8];
+        output.list[7] = input.list[7];
+        output.list[8] = input.list[6];
+
+        return output;
+    }
+
+    public static Obstacle Negative(Obstacle input) {
+        Obstacle output = new Obstacle(input);
+
+        output.list[0] = !input.list[2];
+        output.list[1] = !input.list[1];
+        output.list[2] = !input.list[0];
+
+        output.list[3] = !input.list[5];
+        output.list[4] = !input.list[4];
+        output.list[5] = !input.list[3];
+
+        output.list[6] = !input.list[8];
+        output.list[7] = !input.list[7];
+        output.list[8] = !input.list[6];
+
+        return output;
+    }
+
+}
